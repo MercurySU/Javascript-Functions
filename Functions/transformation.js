aabce46479e395dd7aeea93fe04fd1cfc954e3ae
Suzana <suzanabr039@gmail.com>
2023-01-20 09:27:10 -0300

    functions

---
+++ Functions/transformation.js
@@ -0,0 +1,10 @@
+const numera = 34; 
+const emNumera = new String(numera) //transforma em string
+console.log(emNumera)
+
+const suzana = "sUZana";
+console.log(suzana.charAt(1)) //mostra a letra no índice respectivo
+console.log(suzana.indexOf("a")) //mostra o índice na letra respectiva
+
+console.log(suzana.toUpperCase()) //transforma em maiusculas
+console.log(suzana.toLowerCase()) //transforma em minusculas
